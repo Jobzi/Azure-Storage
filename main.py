b10f4acfb29dd8fc58abb76f3e95a59bdb3eb361
Jobzi <jipsonmurillo@gmail.com>
2021-11-16 09:14:59 -0500

    rema file name

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+#from fastapi.middleware.cors import CORSMiddleware
 from file_stream import FileStream
 import json
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 @app.get("/")
 def read_root():
