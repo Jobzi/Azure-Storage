8215caf798634f7f14d422d184c3701802ba4501
Jobzi <jipsonmurillo@gmail.com>
2021-11-18 10:37:26 -0500

    change import middleware

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from fastapi import FastAPI
-#from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 from file_stream import FileStream
 import json
 app = FastAPI()
 
-#origins = ["*"]
-#app.add_middleware(
-#    CORSMiddleware,
-#    allow_origins=origins,
-#    allow_credentials=True,
-#    allow_methods=["*"],
-#    allow_headers=["*"],
-#)
+app.add_middleware(
+CORSMiddleware,
+allow_origins=["*"], # Allows all origins
+allow_credentials=True,
+allow_methods=["*"], # Allows all methods
+allow_headers=["*"], # Allows all headers
+)
 
 @app.get("/")
 def read_root():
